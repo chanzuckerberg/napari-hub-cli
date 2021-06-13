6925a9a84519411f6df356747d1a24c67dc9fa10
Draga Doncila Pop <ddon0001@student.monash.edu>
2021-06-13 11:25:57 +1000

    Assign simple setup cfg to metadata dictionary

--- napari_hub_cli/napari_hub_cli.py
+++ napari_hub_cli/napari_hub_cli.py
@@ -3,22 +3,24 @@ import re
 from itertools import repeat
 from yaml import full_load
 from configparser import ConfigParser
+from collections import defaultdict
 from .utils import (
     SETUP_META,
     YML_PTH,
     SETUP_CFG_PTH,
     SETUP_PY_PTH,
     YML_INFO,
-    SETUP_INFO,
+    SETUP_CFG_INFO,
     SETUP_COMPLEX_META,
     SETUP_COMPLEX_SOURCES,
     FIELDS,
-    SETUP_PY_SOURCES
+    # SETUP_PY_SOURCES
 )
 import parsesetup
 
+
 def load_meta(pth):
-    meta_dict = dict(zip(FIELDS, repeat(None)))
+    meta_dict = defaultdict(lambda: None)
 
     yml_pth = pth + YML_PTH
     if os.path.exists(yml_pth):
@@ -42,18 +44,22 @@ def read_yml_config(meta_dict, yml_path):
                     meta_dict[field_name] = yml_meta[section][key]
                 elif not key:
                     meta_dict[field_name] = yml_meta[section]
-        for k, v in meta_dict.items():
-            print(f'{k} : {v}')
-
 
 
 def read_setup_cfg(meta_dict, setup_path):
     c_parser = ConfigParser()
     c_parser.read(setup_path)
 
+    for field, (section, key) in SETUP_CFG_INFO:
+        if section in c_parser.sections():
+            if key in c_parser[section]:
+                if meta_dict[field] is None:
+                    meta_dict[field] = c_parser[section][key]
+
+
 def read_setup_py(meta_dict, setup_path):
     setup_args = parsesetup.parse_setup(os.path.abspath(setup_path), trusted=True)
-            
+
 
 def format_meta(meta):
     pass
--- napari_hub_cli/utils.py
+++ napari_hub_cli/utils.py
@@ -4,26 +4,40 @@ SETUP_PY_PTH = "/setup.py"
 
 YML_META = [
     "Authors",
+    "Project Site",
     "Documentation",
     "User Support",
     "Twitter",
     "Source Code",
     "Report Issues",
 ]
-YML_SOURCES = [('authors', None)] + \
-            [('project_urls', field) for field in YML_META[1:-1]] +\
-            [('project_urls', 'Bug Tracker')]
+YML_SOURCES = (
+    [("authors", None)]
+    + [("project_urls", field) for field in YML_META[1:-1]]
+    + [("project_urls", "Bug Tracker")]
+)
 YML_INFO = list(zip(YML_META, YML_SOURCES))
 
-SETUP_META = [
+SETUP_META = YML_META + [
     "Name",
     "Summary",
     "License",
     "Python Version",
 ]
-SETUP_PY_SOURCES = ["name", "description", "license", "python_requires"]
-SETUP_CFG_SOURCES = [("metadata", field) for field in SETUP_PY_SOURCES]
-SETUP_INFO = zip(SETUP_META, SETUP_CFG_SOURCES, SETUP_PY_SOURCES)
+SETUP_CFG_SOURCES = (
+    [("metadata", "author")]
+    + [("metadata", "url")]
+    + YML_SOURCES[2:]
+    + [("metadata", "name")]
+    + [("metadata", "summary")]
+    + [("metadata", "license")]
+    + [("options", "python_requires")]
+)
+SETUP_CFG_INFO = list(zip(SETUP_META, SETUP_CFG_SOURCES))
+
+# SETUP_PY_SOURCES = ["name", "description", "license", "python_requires"]
+# SETUP_CFG_SOURCES = [("metadata", field) for field in SETUP_PY_SOURCES]
+# SETUP_INFO = zip(SETUP_META, SETUP_CFG_SOURCES, SETUP_PY_SOURCES)
 
 SETUP_COMPLEX_META = [
     "Operating System",
@@ -33,10 +47,10 @@ SETUP_COMPLEX_META = [
     "Description",
 ]
 SETUP_COMPLEX_SOURCES = [
-    'classifiers',
-    'install_requires',
-    'version',
-    'long_description'
+    "classifiers",
+    "install_requires",
+    "version",
+    "long_description",
 ]
 
 FIELDS = YML_META + SETUP_META + SETUP_COMPLEX_META
