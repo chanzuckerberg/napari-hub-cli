1e61ddd07aca9562399f3771323dd02e048970df
aranega <vincent.aranega@gmail.com>
2023-05-03 08:25:04 -0600

    CTC-19 Add project quality checklist

---
+++ napari_hub_cli/checklist/projectquality.py
@@ -0,0 +1,41 @@
+from ..fs import NapariPlugin
+from .metadata import MetaFeature, Requirement
+
+NODOC = ""
+
+HAS_SUPPORT_WIN = MetaFeature("Has Windows support", "has_windows_support")
+HAS_SUPPORT_LINX = MetaFeature("Has Linux support", "has_windows_support")
+HAS_SUPPORT_MACOS = MetaFeature("Has MacOS support", "has_windows_support")
+INSTALLABLE_WIN = MetaFeature("Installable on windows", "installable_windows")
+INSTALLABLE_LINUX = MetaFeature("Installable on Linux", "installable_linux")
+INSTALLABLE_MACOS = MetaFeature("Installable on MacOS", "installable_macos")
+ALL_WHEELS_WIN = MetaFeature("All deps are wheels for Windows", "allwheel_windows")
+ALL_WHEELS_LINUX = MetaFeature("All deps are wheels for Linux", "allwheel_linux")
+ALL_WHEELS_MACOS = MetaFeature("All deps are wheels for MacOS", "allwheel_macos")
+HAS_C_EXT_WIN = MetaFeature("Has deps with C extensions", "has_C_ext_windows")
+HAS_C_EXT_LINUX = MetaFeature("Has deps with C extensions", "has_C_ext_linux")
+HAS_C_EXT_MACOS = MetaFeature("Has deps with C extensions", "has_C_ext_macos")
+
+
+def project_quality_check(plugin_repo: NapariPlugin):
+    requirements = plugin_repo.requirements
+    return [
+        Requirement(
+            features=[
+                HAS_SUPPORT_WIN,
+                HAS_SUPPORT_LINX,
+                HAS_SUPPORT_MACOS,
+                INSTALLABLE_WIN,
+                INSTALLABLE_LINUX,
+                INSTALLABLE_MACOS,
+                ALL_WHEELS_WIN,
+                ALL_WHEELS_LINUX,
+                ALL_WHEELS_MACOS,
+                HAS_C_EXT_WIN,
+                HAS_C_EXT_LINUX,
+                HAS_C_EXT_MACOS,
+            ],
+            main_files=[requirements],
+            fallbacks=[],
+        ),
+    ]
