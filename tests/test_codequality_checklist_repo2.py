03d2babc18a76d659e94066cf7772f8a5662c196
aranega <vincent.aranega@gmail.com>
2023-05-03 09:52:48 -0600

    CTC-19 Add test-suite test for code quality

---
+++ tests/test_codequality_checklist_repo2.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import pytest
+
+from napari_hub_cli.autofix import build_issue_message
+from napari_hub_cli.checklist.projectquality import project_quality_suite
+from napari_hub_cli.checklist import (
+    analyse_local_plugin,
+    display_checklist,
+)
+from napari_hub_cli.fs import NapariPlugin
+
+
+@pytest.fixture(scope="module")
+def test_repo():
+    current_path = Path(__file__).parent.absolute()
+    return NapariPlugin(current_path / "resources" / "CZI-29-faulty")
+
+
+# smoke test
+def test_display_checklist(test_repo):
+    result = analyse_local_plugin(test_repo.path, project_quality_suite)
+    display_checklist(result)
