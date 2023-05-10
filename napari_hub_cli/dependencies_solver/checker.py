822041cafbdb05b0427d832a15190daa2ac33562
aranega <vincent.aranega@gmail.com>
2023-05-10 10:57:38 -0600

    CTC-19 Add new entry to check if unexpected error happened

--- napari_hub_cli/dependencies_solver/checker.py
+++ napari_hub_cli/dependencies_solver/checker.py
@@ -44,6 +44,7 @@ class InstallationRequirements(ConfigFile):
         if not self.requirements:
             self.requirements = self.data.get("content", "").splitlines()
         self.options_list = self._build_options()
+        self.errors = None
 
     def _build_options(self):
         # Read the classifiers to have python's versions and platforms
@@ -59,11 +60,14 @@ class InstallationRequirements(ConfigFile):
     @lru_cache()
     def solve_dependencies(self, options):
         try:
+            self.errors = None
             return self.solver.solve_dependencies(self.requirements, options)
         except DistributionNotFound:
             return None
         except InstallationSubprocessError:
             return None
+        except Exception as e:
+            self.errors = e
 
     @lru_cache()
     def _get_platform_options(self, platform):
@@ -174,3 +178,7 @@ class InstallationRequirements(ConfigFile):
     @property
     def has_macos_support(self):
         return "macos" in self.platforms
+
+    @property
+    def had_no_unknown_error(self):
+        return self.errors is None
