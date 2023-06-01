5ace298199cdda36f39ac6a8fa1769ef12da192f
aranega <vincent.aranega@gmail.com>
2023-06-01 12:02:11 -0600

    Fix issue with timestamp format in tests

--- tests/test_additional_info.py
+++ tests/test_additional_info.py
@@ -12,7 +12,6 @@ def test_real_repo():
     return NapariPlugin(current_path / "resources" / "licenses" / "repo_example1")
 
 
-@pytest.mark.online
 def test_existent_cli_tool_version(test_real_repo):
     # Test the get_cli_tool_version value
     additional_info = test_real_repo.additional_info
@@ -20,7 +19,6 @@ def test_existent_cli_tool_version(test_real_repo):
     assert cli_tool_version is not None
 
 
-@pytest.mark.online
 def test_get_cli_tool_version(test_real_repo):
     # Test the get_cli_tool_version property
     additional_info = test_real_repo.additional_info
@@ -28,7 +26,6 @@ def test_get_cli_tool_version(test_real_repo):
     assert isinstance(cli_tool_version, str)
 
 
-@pytest.mark.online
 def test_timestamp(test_real_repo):
     # Test the timestamp property
     additional_info = test_real_repo.additional_info
@@ -37,7 +34,6 @@ def test_timestamp(test_real_repo):
     assert formatted_date.startswith(datetime.now().strftime("%d %b %Y"))
 
 
-@pytest.mark.online
 def test_existent_timestamp(test_real_repo):
     # Test the timestamp value
     additional_info = test_real_repo.additional_info
@@ -46,9 +42,7 @@ def test_existent_timestamp(test_real_repo):
     assert "202" in timestamp
 
 
-@pytest.mark.online
 def test_inherited_properties(test_real_repo):
     # Test the inherited properties from RepositoryFile
     additional_info = test_real_repo.additional_info
-    assert additional_info.exists is not None
-    assert isinstance(additional_info.exists, bool)
+    assert additional_info.exists is True
