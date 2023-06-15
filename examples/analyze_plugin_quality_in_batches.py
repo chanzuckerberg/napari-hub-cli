d738fd9f6a50741826fc5ac3bc0b2bbdd7fb02f2
Sean Martin <seankieran.m@hotmail.com>
2023-06-15 22:04:19 +0100

    Add timeout and dont analyse previous plugins

--- examples/analyze_plugin_quality_in_batches.py
+++ examples/analyze_plugin_quality_in_batches.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import time
 import requests
 import traceback
+import multiprocessing
 
 from napari_hub_cli.utils import get_all_napari_plugin_names
 from napari_hub_cli.checklist.analysis import (
@@ -15,44 +16,85 @@ from napari_hub_cli.checklist.analysis import (
 from napari_hub_cli.checklist.projectquality import project_quality_suite
 
 
+def batch_wrapper(plugin_name, temp_dir, no_pip, result_queue):
+    try:
+        print(f"Analysing plugin {plugin_name}")
+        result = analyse_remote_plugin(
+            plugin_name=plugin_name,
+            display_info=False,
+            directory=temp_dir,
+            requirements_suite=project_quality_suite,
+            disable_pip_based_requirements=no_pip,
+        )
+        result_queue.put(result)
+    except Exception as e:
+        result_queue.put(e)
+    return result
+
+
 def batch_plugin_names(all_plugins, batch_size=10):
     """Yield successive batch-size chunks from all_plugins."""
     for i in range(0, len(all_plugins), batch_size):
         yield all_plugins[i : i + batch_size]
 
 
+def find_aleady_analysed_plugins(output_dir):
+    """Find the plugins that have already been analysed"""
+    csv_files = Path(output_dir).glob("*.csv")
+    analysed_plugins = set()
+    for csv_file in csv_files:
+        with open(csv_file, newline="") as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                analysed_plugins.add(row[0])
+    return analysed_plugins
+
+
 def perform_batched_analysis(
-    batched_names, output_dir, temp_dir="temp_dir", no_pip=False
+    batched_names,
+    output_dir,
+    temp_dir="temp_dir",
+    no_pip=False,
+    overwrite=False,
+    timeout=180,
 ):
     """Perform the analysis on the batched plugins"""
     if not Path(output_dir).exists():
         Path(output_dir).mkdir(parents=True, exist_ok=True)
     date = datetime.now().strftime("%Y-%m-%d_%H-%M")
     errors_filepath = Path(output_dir) / f"errors_{date}.txt"
-    with open(errors_filepath, "w") as f:
-        for i, plugin_names in enumerate(batched_names):
-            results_dict = {}
-            for plugin_name in plugin_names:
-                while not ensure_github_api_rate_limit():
-                    print("Github api rate limit exceeded, waiting 20 minutes")
-                    time.sleep(20 * 60)
-                try:
-                    print(f"Analysing plugin {plugin_name}")
-                    result = analyse_remote_plugin(
-                        plugin_name=plugin_name,
-                        display_info=False,
-                        directory=temp_dir,
-                        requirements_suite=project_quality_suite,
-                        disable_pip_based_requirements=no_pip,
-                    )
-                except Exception as e:
-                    print(f"Error in batch {i}, plugin {plugin_name}: {e}")
-                    f.write(f"Error in batch {i}, plugin {plugin_name}: {e}\n")
-                    traceback.print_exc(file=f)
-                    f.write("\n"
-                            "------------------------------------------------------------------\n")
-                    continue
-                results_dict[plugin_name] = result
+    analysed = find_aleady_analysed_plugins(output_dir)
+    for i, plugin_names in enumerate(batched_names):
+        results_dict = {}
+        for plugin_name in plugin_names:
+            if plugin_name in analysed and not overwrite:
+                print(
+                    f"Skipping plugin {plugin_name} as it has already been analysed"
+                )
+                continue
+            while not ensure_github_api_rate_limit():
+                print("Github api rate limit exceeded, waiting 20 minutes")
+                time.sleep(20 * 60)
+            result_queue = multiprocessing.Queue()
+            process = multiprocessing.Process(
+                target=batch_wrapper,
+                args=(plugin_name, temp_dir, no_pip, result_queue),
+            )
+            process.start()
+            process.join(timeout)
+            if process.is_alive():
+                process.terminate()
+                process.join()
+            result = result_queue.get()
+            if isinstance(result, Exception):
+                with open(errors_filepath, "a") as f:
+                    print(f"Plugin {plugin_name} failed with error {result}")
+                    f.write(f"Plugin {plugin_name} failed with error {result}\n")
+                    f.write(traceback.format_exc())
+                    f.write("\n------------------\n")
+            continue
+                    
+            results_dict[plugin_name] = result
 
             rows = build_csv_dict(results_dict)
             write_csv(rows, Path(output_dir) / f"batched_analysis_{i}.csv")
@@ -120,6 +162,19 @@ def main():
         default="temp_dir",
         help="Directory to store the temporary cloned repositories",
     )
+    parser.add_argument(
+        "--overwrite",
+        "-w",
+        action="store_true",
+        help="Overwrite existing csv files in the output directory",
+    )
+    parser.add_argument(
+        "--timeout",
+        "-to",
+        type=int,
+        default=180,
+        help="Timeout in seconds for each plugin analysis",
+    )
     args = parser.parse_args()
     if args.merge_only:
         merge_csvs(args.output_dir)
@@ -128,7 +183,12 @@ def main():
     all_plugins = list(get_all_napari_plugin_names())
     batched_plugins = batch_plugin_names(all_plugins, batch_size=args.batch_size)
     perform_batched_analysis(
-        batched_plugins, args.output_dir, args.temp_dir, args.no_pip
+        batched_plugins,
+        args.output_dir,
+        args.temp_dir,
+        args.no_pip,
+        args.overwrite,
+        args.timeout,
     )
     merge_csvs(args.output_dir)
 
