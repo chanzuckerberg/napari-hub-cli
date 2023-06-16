e5c05ac39e98d39e0c2ae58470fa184e13d27407
Sean Martin <seankieran.m@hotmail.com>
2023-06-16 16:36:47 +0100

    Add examples for debug

---
+++ examples/reproduce_issues.py
@@ -0,0 +1,26 @@
+from napari_hub_cli.checklist.analysis import analyse_remote_plugin
+from napari_hub_cli.checklist.projectquality import project_quality_suite
+import traceback
+
+PLUGINS = ["napari-LF", "napari-cookiecut", "disease-classifier"]
+
+
+def main(plugins, temp_dir="temp", no_pip=False):
+    for plugin in plugins:
+        print(f"Anlaysing plugin {plugin}")
+        try:
+            analyse_remote_plugin(
+                plugin,
+                display_info=False,
+                directory=temp_dir,
+                disable_pip_based_requirements=no_pip,
+                requirements_suite=project_quality_suite,
+            )
+        except Exception as e:
+            print(f"Error analysing plugin {plugin}: {e}")
+            traceback.print_exc()
+            continue
+
+
+if __name__ == "__main__":
+    main(PLUGINS)
