0b1b986315289d4ec79d302401045ee3ae443cba
aranega <vincent.aranega@gmail.com>
2023-06-21 12:09:57 -0600

    CTC-68 Pass GH token to the calls to gh api

--- napari_hub_cli/fs/license.py
+++ napari_hub_cli/fs/license.py
@@ -4,6 +4,8 @@ from functools import lru_cache
 import requests
 from requests.exceptions import HTTPError
 
+from ..utils import build_gh_header
+
 from ..fs import RepositoryFile
 
 
@@ -63,7 +65,7 @@ class License(RepositoryFile):
             api_url = url.replace(
                 "https://github.com/", "https://api.github.com/repos/"
             )
-            response = requests.get(f"{api_url}/license")
+            response = requests.get(f"{api_url}/license", headers=build_gh_header())
             if response.status_code == 403:  # rate limit exceed
                 print(f"{response.status_code} Client Error: {response.reason} for url: {response.url}")
                 exit(-127)
