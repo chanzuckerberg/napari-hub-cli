97eca2d84be4c5b62cb32c6d3e9ee6b31e6b5946
aranega <vincent.aranega@gmail.com>
2023-06-21 12:05:12 -0600

    CTC-68 Add header building for using GH token

--- tests/test_license.py
+++ tests/test_license.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from unittest import mock
 
@@ -5,6 +6,7 @@ import pytest
 from requests import HTTPError
 
 from napari_hub_cli.fs import NapariPlugin
+from napari_hub_cli.utils import build_gh_header, read_gh_token
 
 RESOURCES = Path(__file__).parent / "resources"
 MOCK_REQUESTS = None
@@ -205,3 +207,22 @@ def test_is_osi_approved_false(test_repo):
             license, "get_osi_approved_licenses", return_value=["MIT"]
         ):
             assert license.is_osi_approved == False
+
+
+def test_access_gh_token():
+    old_value = os.environ["GITHUB_TOKEN"]
+
+    os.environ["GITHUB_TOKEN"] = "MYTOK"
+
+    assert read_gh_token() == "MYTOK"
+    assert build_gh_header() == {
+        'Authorization': 'Bearer MYTOK'
+    }
+
+    del os.environ["GITHUB_TOKEN"]
+
+    assert read_gh_token() is None
+    assert build_gh_header() == {}
+
+    os.environ["GITHUB_TOKEN"] = old_value
+
