5af4c71241334ec18b097afc9f86c528f35e17e3
Simão sá <simao@metacell.us>
2023-08-03 10:13:38 +0100

    fixed import and print

--- napari_hub_cli/checklist/analysis.py
+++ napari_hub_cli/checklist/analysis.py
@@ -6,8 +6,6 @@ from operator import index
 from pathlib import Path
 from textwrap import dedent
 from rich import print
-from rich.console import Console
-import os
 
 import requests
 from git import GitCommandError
@@ -294,7 +292,5 @@ def write_csv(rows, output_filename):
         writer = csv.DictWriter(csvfile, fieldnames=headers)
         writer.writeheader()
         writer.writerows(rows)
-        console = Console()
-    current_path = os.getcwd()
-    csv_path = current_path + '/' + output_filename
-    console.print(f"\n CSV: {output_filename} successfully created at {csv_path} \n", style="bold white")
+    output_path = Path(output_filename)
+    print(f"\n [bold white] CSV: {output_filename} successfully created at {output_path.resolve()} \n [/bold white]" )
